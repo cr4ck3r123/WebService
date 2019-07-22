d4b3f5414ad49ce92f31687969f6f709fdfb66a8
cr4ck3r123 <suportewicell@gmail.com>
2019-07-22 14:31:45 -0300

    implmentando marcas

---
+++ src/main/java/com/aw/api/os/apirest/repository/ModeloRepository.java
@@ -0,0 +1,19 @@
+package com.aw.api.os.apirest.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.aw.api.os.apirest.modelo.Modelo;
+
+public interface ModeloRepository extends JpaRepository<Modelo, Long>{
+
+	//Usuario findAll(String login, String senha);
+	
+	Modelo findById(int id);
+	
+	Modelo findByNome(String nome);
+	
+	List<Modelo> findAll();
+
+}
---
+++ src/main/java/com/aw/api/os/apirest/resources/ModeloResorces.java
@@ -0,0 +1,50 @@
+package com.aw.api.os.apirest.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.aw.api.os.apirest.modelo.Modelo;
+import com.aw.api.os.apirest.repository.ModeloRepository;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+
+@RestController
+@RequestMapping(value="/api")
+@Api(value = "Api Veiculo")
+@CrossOrigin(origins = ("*"))
+public class ModeloResorces {
+	
+	@Autowired
+	ModeloRepository modeloRepository;
+	
+	@GetMapping("/listaModelo")
+	@ApiOperation("Lista Modelos")
+	public List<Modelo> listaModelos(){
+		return modeloRepository.findAll();
+	}
+	
+	@GetMapping("/modelo/{id}")
+	@ApiOperation("Lista Modelo por id")
+	public Modelo listaMarcaId(@PathVariable(value="id") int id){
+		return modeloRepository.findById(id);
+	}
+
+	@GetMapping("/modeloNome/{nome}")
+	@ApiOperation("Lista Modelo por nome")
+	public Modelo listaModeloNome(@PathVariable(value="nome") String nome){
+		return modeloRepository.findByNome(nome);
+	}
+
+	
+	
+
+}
