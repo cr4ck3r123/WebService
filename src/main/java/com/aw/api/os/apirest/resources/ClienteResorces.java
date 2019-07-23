89df039fb49bca904bddcd530f6619afbefc651f
cr4ck3r123 <suportewicell@gmail.com>
2019-07-23 10:23:00 -0300

    banco

--- src/main/java/com/aw/api/os/apirest/resources/ClienteResorces.java
+++ src/main/java/com/aw/api/os/apirest/resources/ClienteResorces.java
@@ -3,6 +3,12 @@ package com.aw.api.os.apirest.resources;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -55,8 +61,10 @@ public class ClienteResorces {
 	clienteRepository.delete(user);
 	}
 	
-	@DeleteMapping("/delclienteid/{id}")
-	@ApiOperation("Deletar clientes")
+	@DELETE
+	@Path("delete/{idcliente}/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
 	public String remover(@PathVariable(value="id") int id) throws Exception {
 			String msg = "";
 			try {
