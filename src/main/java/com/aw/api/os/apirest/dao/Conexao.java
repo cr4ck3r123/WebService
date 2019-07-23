e2a00a27eeb0fdc6f0299f8984ee6c0baa401b71
cr4ck3r123 <suportewicell@gmail.com>
2019-07-23 10:15:47 -0300

    banco

--- src/main/java/com/aw/api/os/apirest/dao/Conexao.java
+++ src/main/java/com/aw/api/os/apirest/dao/Conexao.java
@@ -18,9 +18,10 @@ public class Conexao {
     public static Connection conector() {
         java.sql.Connection conexao = null;
         //A LINHA ABAIXO CHAMA O DRIVER 
-        String driver = "com.mysql.jdbc.Driver";
+        String driver = "org.postgresql.Driver";
         // ARMAZENANDO INFORMA��ES REFERENTES AO BANCO DE DADOS
-        String url = "ec2-50-16-197-244.compute-1.amazonaws.com/d91bkl3n4oqfpf";
+        String url = "postgres://zkwhyymawqbzpd:94b6dae63adedc831c33c4e6b4173443b8a7d3bba70c659c1d2077d1297924e6@ec2-50-16-197-244.compute-1.amazonaws.com:5432/d91bkl3n4oqfpf\r\n" + 
+        		"";
         String user = "zkwhyymawqbzpd";
         String password = "94b6dae63adedc831c33c4e6b4173443b8a7d3bba70c659c1d2077d1297924e6";
         
@@ -42,7 +43,7 @@ public class Conexao {
         public static Connection conector2() {
             java.sql.Connection conexao = null;
             //A LINHA ABAIXO CHAMA O DRIVER 
-            String driver = "com.mysql.jdbc.Driver";
+            String driver = "org.postgresql.Driver";
             // ARMAZENANDO INFORMA��ES REFERENTES AO BANCO DE DADOS
             String url = "postgres://zkwhyymawqbzpd:94b6dae63adedc831c33c4e6b4173443b8a7d3bba70c659c1d2077d1297924e6@ec2-50-16-197-244.compute-1.amazonaws.com:5432/d91bkl3n4oqfpf\r\n" + 
             		"";
